0f4f88d3ad8fcecdc9f8385194142793233e7c20
nithinkumar2468 <nithinkumarkodipyaka@gmail.com>
2024-05-24 20:06:50 +0530

    deploy

--- src/layout/SearchResults.js
+++ src/layout/SearchResults.js
@@ -1,14 +1,18 @@
 import "./SearchResults.css";
 import Searchlist from "./Searchlist";
 
-function SearchResults({ results }) {
-    return (
+function SearchResults({results}){
+    return(
         <div className="results-body">
-            <div className="results-list">
-                <Searchlist users={results} />
-            </div>
-        </div>
-    );
+    <div className="results-list">
+        {
+            results.map((result,id)=>
+            {
+                return <Searchlist result={result} key={id}/>
+            })
+        }
+    </div>
+    </div>
+    )
 }
-
 export default SearchResults;
--- src/layout/Searchlist.js
+++ src/layout/Searchlist.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Searchlist.css";
 import { Link } from "react-router-dom";
 
-function Searchlist({ users }) {
+function Searchlist({ result }) {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        loadUsers();
+    }, []);
+
+    const loadUsers = async () => {
+        const results = await axios.get("https://stscrmbackend-production.up.railway.app/api/n1/products");
+        setUsers(results.data);
+        
+    };
     return (
-        <div className="searchlist">
-            {users.map((user) => (
-                <Link key={user.id} to={`/buynowproduct/${user.id}`} style={{ textDecoration: 'none' }}>
-                    {user.pname.slice(0, 30)}
-                </Link>
+        <div className="searchlist" >
+            {users.map((user, index) => (
+            <Link to={`/buynowproduct/${result.id}`} style={{"textDecoration":'none'}}>
+                {result.pname.slice(0, 30)}
+            </Link>
             ))}
         </div>
-    );
+    )
 }
-
 export default Searchlist;
